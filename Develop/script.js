76c65428afb69e0ae32d70406f077f4ddad7eff3
John Kim <johnk9000@gmail.com>
2020-06-18 13:13:12 -0500

    Random integer generator with array output

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var char1 = "A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+var char1vect = char1.split(", ");
+
+function RandomInt(max) {
+  return Math.floor( Math.random() * Math.floor(max));
+}
+
+console.log(char1vect);
+//building RNG to
+for(i = 0; i < 10; i++) {
+var entry = RandomInt(char1vect.length - 1);
+console.log(entry);
+console.log(char1vect[entry]);
+}
